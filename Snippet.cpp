f2258f532489ba53b2156b64d07f39390e67e7d4
Quang Mac <175633189+Xenixius@users.noreply.github.com>
2024-10-25 22:41:01 +0700

    Create Snippet.cpp

---
+++ Snippet.cpp
@@ -0,0 +1,17 @@
+// Horizon
+#include <bits/stdc++.h>
+#define ll long long
+#define ft(i, l, r, d) for (int i = l; i < r; i += d)
+#define fd(i, r, l, d) for (int i = r; i > l; i -= d)
+#define ftf(i, l, r, d) for (int i = l; i <= r; i += d)
+#define fdf(i, r, l, d) for (int i = r; i >= l; i -= d)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define File(name) freopen(name".INP", "r", stdin); freopen(name".OUT", "w", stdout);
+using namespace std;
+
+int main() {
+    //File("IOFILE");
+}
