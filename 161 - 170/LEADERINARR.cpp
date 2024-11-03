14d3815c374b9162848ee5a72f66866ad911dc30
Quang Mac <175633189+Xenixius@users.noreply.github.com>
2024-11-03 16:01:01 +0700

    Update and rename LEADERLNARR.cpp to LEADERINARR.cpp

--- 161 - 170/LEADERINARR.cpp
+++ 161 - 170/LEADERINARR.cpp
@@ -1,11 +1,12 @@
+// Horizon
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
 
 main() {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    freopen("LEADERLNARR.INP", "r", stdin);
-    freopen("LEADERLNARR.OUT", "w", stdout);
+    freopen("LEADERINARR.INP", "r", stdin);
+    freopen("LEADERINARR.OUT", "w", stdout);
 
     int t;
     cin >> t;
